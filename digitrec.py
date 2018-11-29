525dac95f38963b99a9985a8053e4a699513b3af
conor <c-raftery@hotmail.com>
2018-11-29 01:27:32 +0000

    preprocessing done

--- digitrec.py
+++ digitrec.py
@@ -11,3 +11,14 @@ from keras.datasets import mnist
 
 # load data
 (x_train,y_train), (x_test,y_test) = mnist.load_data()
+
+# preprocessing
+x_test = x_test.reshape(x_test.shape[0], 784)
+x_train = x_train.reshape(x_train.shape[0], 784)
+x_test = x_test.astype('float32')
+x_train = x_train.astype('float32')
+
+x_test /= 255
+x_train /= 255
+y_test = keras.utils.to_categorical(y_test, 10)
+y_train = keras.utils.to_categorical(y_train, 10)
