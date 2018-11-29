5df6997339f2d68d2e8763dfc0c7a33a280f7b55
conor <c-raftery@hotmail.com>
2018-11-29 01:25:29 +0000

    imports done, loading data

---
+++ digitrec.py
@@ -0,0 +1,13 @@
+# import required modules
+import keras
+import sys
+import cv2
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+# import MNIST dataset
+from keras.datasets import mnist
+
+# load data
+(x_train,y_train), (x_test,y_test) = mnist.load_data()
